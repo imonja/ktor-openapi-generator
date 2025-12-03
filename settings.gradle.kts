884da0b909887d3f07e8fef678fd6ac8175a5317
Yuriy Monachinskiy <ym@letyshops.com>
2025-12-03 13:33:42 +0200

    First release (BUMP)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,6 @@ dependencyResolutionManagement {
     // Felles for alle gradle prosjekter i repoet
     @Suppress("UnstableApiUsage")
     repositories {
-        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
         mavenCentral()
         mavenLocal()
     }
