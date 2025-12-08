9922ed5f6b8f69b27dacbe9a5f11fae0570c48fb
Yuriy Monachinskiy <ym@letyshops.com>
2025-12-08 10:02:15 +0200

    Disable automatic release in Maven Central publishing configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -118,7 +118,7 @@ publishing {
 
 // Maven Central publishing configuration
 mavenPublishing {
-    publishToMavenCentral(automaticRelease = true)
+    publishToMavenCentral()
     signAllPublications()
 
     coordinates(
